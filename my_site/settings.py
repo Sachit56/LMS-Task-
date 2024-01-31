c4376e9c08d66651fd457e07b57e19ee71cee08b
Sachit Khadka <sachitkhadka73@gmail.com>
2024-01-31 16:46:51 +0545

    postgres connection

--- my_site/settings.py
+++ my_site/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER':'postgres',
+        'PASSWORD':'admin@123',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
