e53fa233ca9bfa9d25abf1d80a0b8cd4d29afeea
Sachit Khadka <sachitkhadka73@gmail.com>
2024-02-01 17:38:02 +0545

    models

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class UserModel(models.Model):
+    userid=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=254,unique=True)
+    membership_date=models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return str(self.id)
+
+class BookModel(models.Model):
+    bookid=models.AutoField(primary_key=True)
+    title=models.CharField( max_length=50)
+    isbn=models.CharField(unique=True,max_length=50)
+    published_date=models.DateField(auto_now=False, auto_now_add=False)
+    genre=models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return str(self.id)
+    
+class BookDetailsModel(models.Model):
+    detailsid=models.AutoField(primary_key=True)
+    bookid=models.ForeignKey("BookModel", verbose_name=("Book Id"), on_delete=models.CASCADE)    
+    number_of_pages=models.IntegerField()
+    publisher=models.CharField(max_length=50)
+    language=models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.id)
+    
+class BorrowedBooksModel(models.Model):
+    userid=models.ForeignKey("UserModel", verbose_name=("User Id"), on_delete=models.CASCADE)  
+    bookid=models.ForeignKey("BookModel", verbose_name=("Book Id"), on_delete=models.CASCADE) 
+    borrowed_date=models.DateField(auto_now=False, auto_now_add=False)   
+    return_date=models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return str(self.id)
+
+
+    
+
+
+        
--- lms/tests.py
+++ lms/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class FirstTestCase(TestCase):
+
+    def setUp(self):
+        pass
+    def test_equal(self):
+        self.assertEqual(1,1)
