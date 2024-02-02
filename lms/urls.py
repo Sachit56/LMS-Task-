c8593fac796acd0f6c357359667882cd98b66a74
Sachit Khadka <sachitkhadka73@gmail.com>
2024-02-02 17:14:41 +0545

    views all done

--- lms/urls.py
+++ lms/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('users/',views.UserView.as_view(),name='users'),
     path('users/<int:id>/',views.UserIdView.as_view(), name='user-detail'),
     path('books/',views.BookView.as_view(),name='books'),
-    path('books/<int:id>/',views.BookDetailsView.as_view(),name='book_detail')
+    path('books/<int:id>/',views.BookDetailsView.as_view(),name='book_detail'),
+    path('borrow/',views.BorrowedBooksView.as_view(),name='borrowed_book'),
+    # path('return/',views.ReturnBookView.as_view(),name='return')
 ]
 
--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,4 @@
+import datetime
 from urllib import request
 from django.shortcuts import render
 from rest_framework.views import APIView
@@ -120,8 +121,43 @@ class BookDetailsView(APIView):
         except Exception as e:
             return Response({'status': 500, 'message': f'Internal Server Error: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)    
 
-
+#■ Borrow a Book , ■ Return a Book , ■ List All Borrowed Books...........................
 
 class BorrowedBooksView(APIView):
+
+
     def get(self,request):
-        pass
+        try:
+            books=BorrowedBooksModel.objects.all()
+            serializer=BorrowedBooksSerializer(books,many=True)
+
+            return Response({'status':200,'borrowed_books':serializer.data})
+        except:
+            return Response({'status':400,'message':serializer.errors})
+
+    
+    
+    def post(self,request):
+        try:
+            serializer=BorrowedBooksSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+
+                return Response({'status':200,'borrowedbooks_by_user':serializer.data['userid']})
+            else:
+                return Response({'status':400,'message':serializer.errors})
+        except Exception as e:
+            return Response({'status': 500, 'message': f'Internal Server Error: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)    
+    
+    
+    def put(self,request):
+        try:
+            book=BorrowedBooksModel.objects.get(return_date=request.data['return_date'])
+            book.delete()
+
+            return Response({'status':200,'message':'Book returned successfully'})
+        except:
+            return Response({'status':400,'message':'cannot return'})    
+
+
+
