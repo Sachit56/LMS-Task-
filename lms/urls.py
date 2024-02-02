683fc0654076ebdb69b9728e89173bba657aa93b
Sachit Khadka <sachitkhadka73@gmail.com>
2024-02-02 15:18:14 +0545

    books,user,details views

--- lms/urls.py
+++ lms/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('users/',views.UserView.as_view(),name='users'),
+    path('users/<int:id>/',views.UserIdView.as_view(), name='user-detail'),
+    path('books/',views.BookView.as_view(),name='books'),
+    path('books/<int:id>/',views.BookDetailsView.as_view(),name='book_detail')
 ]
 
--- lms/views.py
+++ lms/views.py
@@ -1,14 +1,14 @@
+from urllib import request
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
 from .models import *
 from .serializers import *
-from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
 
-# Create your views here.
+
+
+#■ Create a New User, List All Users...................
 
 class UserView(APIView):
 
@@ -17,9 +17,111 @@ class UserView(APIView):
         serializer=UserSerializer(users,many=True)
 
         return Response({'status':200,'users':serializer.data})
+    
+    
+    def post(self,request):
+        try:
+            serializer=UserSerializer(data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+
+                return Response({'status':200,'users':serializer.data})
+            else:
+                return Response({'status':400,'message':serializer.errors})
+        
+        except Exception as e:
+            return Response({'status': 500, 'message': f'Internal Server Error: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+#■ Get User by ID.......................
+
+class UserIdView(APIView):
+
+
+    def get(self,request,id):
+        try:
+            user=UserModel.objects.get(id=id)
+            serializer=UserSerializer(user)
+
+            return Response({'status':200,'user':serializer.data})
+        except:
+            return Response({'status':400,'message':serializer.errors})
+
+   
+# ■ Add a New Book , ■ List All Books............
+
+class BookView(APIView):
+
+    def get(self,request):
+        try:
+            books=BookModel.objects.all()
+            serializer=BookSerializer(books,many=True)
+
+            return Response({'status':200,'users':serializer.data})
+        except:
+            return Response({'status':400,'message':serializer.errors})
+    
+    
+    def post(self,request):
+        try:
+            serializer=BookSerializer(data=request.data)
+            
+            if serializer.is_valid():
+                serializer.save()
+
+                return Response({'status':200,'books':serializer.data})
+            else:
+                return Response({'status':400,'message':serializer.errors})
+        except Exception as e:
+            return Response({'status': 500, 'message': f'Internal Server Error: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)    
+    
+    
+    def patch(self,request):
+
+        try:    
+            books=BookModel.objects.get(id=request.data['id'])
+            serializer=BookSerializer(books,data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'status':200,'books':serializer.data})
+            else:
+                return Response({'status':400,'message':serializer.errors})
+        except Exception as e:
+            return Response({'status': 500, 'message': f'Internal Server Error: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)    
+        
+
+#■ Get Book by ID , ■ Assign/Update Book Details.............
+
+class BookDetailsView(APIView):
+
+
+    def get(self,request,id):
+        try:
+            book=BookDetailsModel.objects.get(book=id)
+            serializer=BookDetailsSerializer(book)
+
+            return Response({'status':200,'Details':serializer.data})
+        except Exception as e:
+            return Response({'status': 500, 'message': f'Internal Server Error: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)    
+    
+    
+    def put(self,request,id):
+        try:
+            book=BookDetailsModel.objects.get(book=id)
+            serializer=BookDetailsSerializer(book,data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+
+                return Response({'status':200,'Details':serializer.data})
+            else:
+                return Response({'status':400,'message':serializer.errors}) 
+        except Exception as e:
+            return Response({'status': 500, 'message': f'Internal Server Error: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)    
 
 
 
-
-
-
+class BorrowedBooksView(APIView):
+    def get(self,request):
+        pass
